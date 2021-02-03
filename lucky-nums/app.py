7995544262050c54d7985bdc81ed5537a6fd5a64
Aaron Brant <72094000+KortesmakiAB@users.noreply.github.com>
2021-02-03 11:36:04 -0500

    initial LuckyNums assessment commits

--- lucky-nums/app.py
+++ lucky-nums/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from random import randint
+import requests
 
 app = Flask(__name__)
 
@@ -8,3 +10,80 @@ def homepage():
     """Show homepage."""
 
     return render_template("index.html")
+
+
+###################################################
+# API helper function
+
+def make_error_dict(request):
+    """Use form data to create error dictionary with nested dicts.
+    Nested dicts contain name, validation conditional, and error message."""
+
+    errors = {
+        "errors": {}
+    }
+
+    name                = request.json.get('name')
+    if not name or not name.strip():
+        errors['errors']['name']  = 'Please enter a valid name.'
+
+    year                = request.json.get('year')
+    year                = year and 1900 <= int(year) <= 2000
+    if not year:
+        errors['errors']['year']  = 'Please enter a year from 1900 to 2000.'
+
+    email               = request.json.get('email')
+    email               = '@' in email and '.' in email
+    if not email:
+        errors['errors']['email']  = 'Please enter a valid email address.'
+
+    color               = request.json.get('color')
+    color               = color in {'red', 'green', 'orange', 'blue'}
+    if not color:
+        errors['errors']['color']  = 'Please choose 1 color: red, green, orange, or purple.'
+
+
+    return errors if errors['errors'].values() else None   
+
+
+###################################################
+# API RESTful ROUTES
+
+@app.route('/api/get-lucky-num', methods = ['POST'])
+def handle_form_submit():
+    """TODO"""
+    
+    errors          = make_error_dict(request)
+
+    if errors:
+        return (jsonify(errors))
+
+
+    rand_num        = randint(1, 100)
+    try:
+        num_resp    = requests.get(f'http://numbersapi.com/{rand_num}/trivia')
+    except:
+        errors['bad request'] = 'Random number trivia not found. Please try again.'
+        return (jsonify(errors))
+
+
+    year            = request.json.get('year')
+    try:
+        yr_resp     = requests.get(f'http://numbersapi.com/{year}/year')
+    except:
+        errors['bad request'] = 'Year trivia not found. Please try again.'
+        return (jsonify(errors))
+
+    trivia = {
+        "num":  {
+            "fact": num_resp.text,
+            "num":  rand_num
+        },
+        "year": {
+            "fact": yr_resp.text,
+            "year": year
+        }        
+    }
+    
+    return jsonify(trivia)
+   
--- lucky-nums/static/lucky.js
+++ lucky-nums/static/lucky.js
@@ -1,11 +1,43 @@
 /** processForm: get data from form and make AJAX call to our API. */
 
-function processForm(evt) {
+async function processForm(evt) {
+    evt.preventDefault();
+
+    const name  = $('#name').val();
+    const year  = $('#year').val();
+    const email = $('#email').val();
+    const color = $('#color').val();
+
+    const formInput = {name, year, email, color};
+    
+    const baseUrl   = 'http://127.0.0.1:5000';
+    const resp      = await axios.post(`${baseUrl}/api/get-lucky-num`, formInput);
+    handleResponse(resp);
+
+    $('#lucky-form').trigger('reset');
 }
 
-/** handleResponse: deal with response from our lucky-num API. */
+
+/** handleResponse from lucky-num API. */
 
 function handleResponse(resp) {
+    if (resp.data.errors){
+        for (let key of Object.keys(resp.data.errors)){
+            $(`#${key}-err`).text(`${resp.data.errors[`${key}`]}`);
+        }
+    }
+    else {
+        const num_txt    = `Your lucky number is ${resp.data.num.num}. Fun fact: ${resp.data.num.fact}`;
+        const yr_txt    = `Fun fact about your birth year ${resp.data.year.year}: ${resp.data.year.fact}`;
+        
+        const numP      = document.createElement('p');
+        const yrP       = document.createElement('p');
+
+        numP.innerText  = num_txt;
+        yrP.innerText   = yr_txt;
+
+        $('#lucky-results').append(numP, yrP);
+    }
 }
 
 
